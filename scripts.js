96dfe724f8d38ed3fac26795dd36b74781c3d632
Zach Boatwright <szboatwright@gmail.com>
2018-04-09 20:43:19 -0500

    ⚡  IMPROVE: Completely redoes JS - much cleaner now

--- scripts.js
+++ scripts.js
@@ -1,39 +1,62 @@
-var renderer = new THREE.WebGLRenderer({canvas: document.getElementById('my-canvas'), antialias: true});
-renderer.setClearColor(0x26b2de); // sets the background color
-renderer.setPixelRatio(window.devicePixelRatio); // sets the pixel ratio to be the size of the screen
-renderer.setSize(window.innerWidth, window.innerHeight); // sets the renderer to fill the window
+// Global Variables
+let scene;
+let camera;
+let renderer;
+let cloud;
 
-let camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000); // Adds new camera w/ FOV of 35, then sets the width and height of the camera to be that of the window, then sets the near and far clipping distances.
+// Constants
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+const CLOUD_MAT = new THREE.MeshLambertMaterial({color: 0xffffff});
 
-let scene = new THREE.Scene(); // adds in the scene
+// Init Scene
+function init() {
+  scene = new THREE.Scene();
+  initCamera();
+  initRenderer();
+  initCloud();
 
-//creates the scene lighting
-let lightAmb = new THREE.AmbientLight(0xffffff, 0.5); // creates an ambient light with the color and intensity
-scene.add(lightAmb);
-let lightAmbBlu = new THREE.AmbientLight(0x26b2de, 0.15);
-scene.add(lightAmbBlu);
+  //creates the scene lighting
+  let lightAmb = new THREE.AmbientLight(0xffffff, 0.5); // creates an ambient light with the color and intensity
+  scene.add(lightAmb);
+  let lightAmbBlu = new THREE.AmbientLight(0x26b2de, 0.15);
+  scene.add(lightAmbBlu);
 
-let lightPnt = new THREE.PointLight(0xffffff, 0.5); // creates an ambient light with the color and intensity
-scene.add(lightPnt);
-lightPnt.position.y = 100;
-lightPnt.position.x = 50;
+  let lightPnt = new THREE.PointLight(0xffffff, 0.5); // creates an ambient light with the color and intensity
+  scene.add(lightPnt);
+  lightPnt.position.y = 100;
+  lightPnt.position.x = 50;
 
-//cloud spawn
-let cloudMat = new THREE.MeshLambertMaterial({color: 0xffffff}); // creates a lambert material that has shading
+  document.body.appendChild(renderer.domElement);
+}
 
-let loader = new THREE.JSONLoader();
-let cloud = null;
-cloud = loader.load('data/cloud1.json', loadCloud);
-function loadCloud (geometry) {
-  let cloud = new THREE.Mesh(geometry, cloudMat);
-  scene.add(cloud);
-  cloud.position.z = -20;
-};
+// Functions
+function initCamera() {
+  camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 10);
+  camera.position.set(0, 0, 7);
+  camera.lookAt(scene.position);
+}
 
+function initRenderer() {
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(WIDTH, HEIGHT);
+  renderer.setClearColor(0x26b2de); // sets the background color
+  renderer.setPixelRatio(window.devicePixelRatio); // sets the pixel ratio to be the size of the screen
+}
 
-// render the scene
-function render() { // renders the scene using the scene and camera objects
-  renderer.render (scene, camera);
+function initCloud() {
+  var loader = new THREE.JSONLoader();
+  loader.load('./data/cloud1.json',function(geometry) {
+    cloud = new THREE.Mesh(geometry, CLOUD_MAT);
+    scene.add(cloud);
+  });
+}
+
+function render() {
   requestAnimationFrame(render);
-};
+  // Animation stuff
+  renderer.render (scene, camera);
+}
+
+init();
 render();
