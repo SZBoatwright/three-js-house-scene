36578ef30ee71b7b69dc875921dbe2da8b1c7302
Zach Boatwright <szboatwright@gmail.com>
2018-04-14 10:33:49 -0500

    ⚡  IMPROVE: lights are now constants

--- scripts.js
+++ scripts.js
@@ -6,9 +6,16 @@ let renderer;
 // Constants
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
-const CLOUD_MAT = new THREE.MeshLambertMaterial({color: 0xffffff});
+
+const CLOUD_MAT = new THREE.MeshLambertMaterial({ color: 0xffffff });
+
 const SPEED_MIN = 3;
 const SPEED_MAX = 75;
+
+// Scene Lighting
+const LIGHT_POINT = new THREE.PointLight(0xffffff, 0.5); // creates an ambient light with the color and intensity
+const LIGHT_AMB = new THREE.AmbientLight(0xffffff, 0.5); // creates an ambient light with the color and intensity
+const LIGHT_AMB_BLU = new THREE.AmbientLight(0x26b2de, 0.15);
 
 // Init Scene
 function init() {
@@ -17,18 +24,15 @@ function init() {
   initRenderer();
   initCloud();
 
-  //creates the scene lighting
-  let lightAmb = new THREE.AmbientLight(0xffffff, 0.5); // creates an ambient light with the color and intensity
-  scene.add(lightAmb);
-  let lightAmbBlu = new THREE.AmbientLight(0x26b2de, 0.15);
-  scene.add(lightAmbBlu);
+  // Adds the scene lighting
+  scene.add(LIGHT_AMB);
+  scene.add(LIGHT_AMB_BLU);
 
-  let lightPnt = new THREE.PointLight(0xffffff, 0.5); // creates an ambient light with the color and intensity
-  scene.add(lightPnt);
-  lightPnt.position.y = 100;
-  lightPnt.position.x = 50;
+  scene.add(LIGHT_POINT);
+  LIGHT_POINT.position.y = 100;
+  LIGHT_POINT.position.x = 50;
 
-  document.body.appendChild(renderer.domElement);
+  document.body.appendChild(renderer.domElement); // Adds in WebGL renderer
 }
 
 // Functions
@@ -39,7 +43,7 @@ function initCamera() {
 }
 
 function initRenderer() {
-  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(WIDTH, HEIGHT);
   renderer.setClearColor(0x26b2de); // sets the background color
   renderer.setPixelRatio(window.devicePixelRatio); // sets the pixel ratio to be the size of the screen
@@ -47,7 +51,7 @@ function initRenderer() {
 
 function initCloud() {
   var loader = new THREE.JSONLoader();
-  loader.load('./data/cloud1.json',function(geometry) {
+  loader.load("./data/cloud1.json", function(geometry) {
     let mesh = new THREE.Mesh(geometry, CLOUD_MAT);
 
     // Give the cloud a random speed between MIN_SPEED and MAX_SPEED
@@ -57,13 +61,13 @@ function initCloud() {
   });
 }
 
-function moveClouds () {
-  scene.children.forEach(function(curVal, i){
+function moveClouds() {
+  scene.children.forEach(function(curVal, i) {
     if (scene.children[i]["type"] == "Mesh") {
       // Moves the cloud based on assigned speed
       scene.children[i].position.x -= scene.children[i].speed;
     }
-  })
+  });
 }
 
 function getRandomInt(min, max) {
@@ -75,7 +79,7 @@ function render() {
   // Animation stuff
   moveClouds();
 
-  renderer.render (scene, camera);
+  renderer.render(scene, camera);
 }
 
 init();
